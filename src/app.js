3565ed81abc91328b1db4d45c188dbf17faed6f2
Vikalp Mishra <mishravikalp896@gmail.com>
2023-07-09 17:31:24 +0530

    linking weather page

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
+
 const app = express();
 const port = process.env.PORT || 8000;
 
-// Public static path
-const publicPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../views');
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
-app.use(express.static(publicPath));
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
 
+// Routes
 app.get('/', (req, res) => {
     res.render('index');
 });
@@ -21,13 +28,16 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
-    res.send('Get information related to the weather of my empire');
+    res.render('weather');
 });
 
 app.get('*', (req, res) => {
-    res.render('404error');
+    res.render('404error', {
+        errorMsg: 'Opps! Page Not Found'
+    })
 });
 
+// Start the server
 app.listen(port, () => {
-    console.log('Listening on port 8000');
+    console.log(`Server is up on port ${port}`);
 });
